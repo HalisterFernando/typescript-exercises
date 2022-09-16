4481ba13c31da0a2173dcdeddc43f4ddc2bf38b1
Seu nome <halisterfernando@hotmail.com>
2022-09-16 18:34:42 -0300

    feat: :rocket: desafio-2

---
+++ desafio-2/index.js
@@ -0,0 +1,7 @@
+// Crie uma variável que recebe um número;
+// Converta este número para string em uma nova variável
+// Esta variável de conversão deve estar tipada por inferência
+// Imprima este número em uma string maior utilizando recurso de template string
+var x = 1;
+var numberToString = x.toString();
+console.log("N\u00FAmero convertido para string ".concat(numberToString));
