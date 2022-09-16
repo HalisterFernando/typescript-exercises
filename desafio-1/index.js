07d6fb180b6db90d175610c45cc5de4d3d122279
Seu nome <halisterfernando@hotmail.com>
2022-09-16 15:19:26 -0300

    feat: :rocket: criada função que retorna soma de dois argumentos do tipo número

--- desafio-1/index.js
+++ desafio-1/index.js
@@ -1,10 +1,7 @@
 // Crie uma função que aceita dois argumentos, somente do tipo número e exibe a soma entre os dois
-
-const x = 10;
-const y = 20;
-
-function sumNumbers(num1: number, num2: number) {
+var x = 10;
+var y = 20;
+function sumNumbers(num1, num2) {
     console.log(num1 + num2);
 }
-
 sumNumbers(x, y);
