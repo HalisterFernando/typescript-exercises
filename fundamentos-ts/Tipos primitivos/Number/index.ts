ebe719e6d1226b3d449a02aca3c495a9710cc663
Seu nome <halisterfernando@hotmail.com>
2022-09-16 15:43:49 -0300

    feat: :rocket: criado exemplos simples para tipagem number

---
+++ fundamentos-ts/Tipos primitivos/Number/index.ts
@@ -0,0 +1,7 @@
+let x: number = 10; // tipagem vem logo após ao nome da variável após o :
+
+x = 'teste' // uma vez definido o tipo da variável, o TS não aceita que o tipo dela seja outro
+x = 16 // variável redeclarada com o mesmo tipo
+
+const y: number = 15.5 // números float também são do tipo number
+
