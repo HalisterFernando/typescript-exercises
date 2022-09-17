a042b36c469af9f5606bb3ef692be0de7a826610
Seu nome <halisterfernando@hotmail.com>
2022-09-17 15:42:57 -0300

    feat: :rocket: exemplos do tipo array

---
+++ fundamentos-ts/Avançando em tipos/Arrays.ts
@@ -0,0 +1,6 @@
+let numbers: number[] = [1, 2, 3]; // Apenas números podem ser inseridos neste array
+let nomes: string[] = ['Halister', 'Priscila']; // Apenas strings podem ser inseridas neste array
+
+// Outra sintaxe para arrays
+
+const nums: Array<number> = [4, 5, 6]
