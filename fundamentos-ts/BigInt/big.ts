3c9bb3120b696847dad63fc5fa6001685c0ac756
Seu nome <halisterfernando@hotmail.com>
2022-09-20 09:47:41 -0300

    feat: :rocket: exemplo com bigint

---
+++ fundamentos-ts/BigInt/big.ts
@@ -0,0 +1,7 @@
+// o tipo bigint é usado para declarar números com valores muito altos;
+// podemos usar a notação literal, exemplo 100n
+// versão ES2020 no tsconfig.json
+
+let n: bigint;
+
+n = 1000n
