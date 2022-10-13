2935731d9404ffe10dc91d082bfa633f21643f20
Seu nome <halisterfernando@hotmail.com>
2022-10-12 22:58:49 -0300

    build: :rocket: tuplas

---
+++ Object Types/Tuplas.ts
@@ -0,0 +1,11 @@
+// Um tipo de array que definimos a quantidade e o tipo de elementos
+// Criamos um novo type e nele inserimos um array com os tipos necessários
+// Cada tipo conta como um elemento
+
+type fiveNumbers = [number, number, number, number, number]
+
+const myNumArr: fiveNumbers = [ 1, 2, 3, 4, 5]
+
+type nameAndAge = [string, number];
+
+const anotherUser: nameAndAge = [ 'halister', 32];
