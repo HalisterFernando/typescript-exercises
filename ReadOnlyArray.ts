cdf9bd7d9a4a588ede77a14c7e9af876c8a20bc6
Seu nome <halisterfernando@hotmail.com>
2022-10-12 22:51:58 -0300

    build: :rocket: read only array

---
+++ ReadOnlyArray.ts
@@ -0,0 +1,5 @@
+// Deixa os items de um array como somente leitura
+// Modificações podem ser feitas somentes através de metodos array
+
+let myArr: ReadonlyArray<string> = ["maçã", "laranja", "banana"];
+
