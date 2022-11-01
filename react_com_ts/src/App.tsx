324df09b4958a7a82fd86f2d4c54a94201e0ac0f
Seu nome <halisterfernando@hotmail.com>
2022-11-01 18:22:15 -0300

    chore: :rocket: criacao de variaveis

--- react_com_ts/src/App.tsx
+++ react_com_ts/src/App.tsx
@@ -1,9 +1,23 @@
 
 
 function App() {
+
+  // variáveis 
+
+  const name: string = "Halister"
+  const age: number = 32
+  const isWorking: boolean = false
+
   return (
     <div className="App">
       <h1> TypeScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (
+        <div>
+          Está trabalhando
+        </div>
+      )}
     </div>
   );
 }
