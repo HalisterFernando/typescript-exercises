8890d1e84f65d7eab46167519f060a45ce996643
Seu nome <halisterfernando@hotmail.com>
2022-11-01 18:28:32 -0300

    chore: :zap: instala typescript snippets e cria componente

---
+++ react_com_ts/src/components/FirstComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const FirstComponent = (props: Props) => {
+  return (
+    <div>
+        <h1>Meu primeiro componente</h1>
+    </div>
+  )
+}
+
+export default FirstComponent
